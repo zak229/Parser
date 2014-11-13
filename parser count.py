050c2a0796be9424107d6204ddc818ad2bb2ba31
zak229 <zak229@nyu.edu>
2014-11-13 13:05:10 -0500

    added extra terms, slight optimize
    optimize:
    divided up urls so that only 100 use nested try catch
    stopped resetting variables to zero on every loop
    
    terms:
    spin off
    spun-off
    spunoff
    spun off

--- parser count.py
+++ parser count.py
@@ -1,4 +1,4 @@
-# Written for Python 3.x
+# Written for Python 3.x (I think urllib will cause issues in Python 2.7.x, I think urllib.request should be changed to urllib2 to work in 2.7.x)
 # Requires BeutifulSoup (bs4)
 from bs4 import BeautifulSoup
 from collections import Counter
@@ -9,6 +9,10 @@ url = "http://www.sec.gov/litigation/litreleases/lr"
 url2 = "http://www.sec.gov/litigation/litreleases/"
 #First case should be 14644
 case = 14644
+#every url ends in .txt until around 16100, it switches back and forth for a while afterword, goes to only .htm after (definitely) 16200.  
+#Going to allow 100 cases to be slower to maximize sample size
+sufswitchcase = 16100
+sufswitchcase2 = 16200
 #this is the last case of 1Q2006 -- last year before year was included in url
 newestcase = 19635
 suffix = ".htm"
@@ -24,17 +28,47 @@ twelve = 22582
 thirteen = 22900
 #as of 11-10-14
 fourteen = 23129
-while case <= newestcase:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
+option = 0
+spindashoff = 0
+spinoff = 0
+spinspaceoff = 0
+spundashoff = 0
+spunoff = 0
+spunspaceoff = 0
+insider = 0
+divest = 0
+fraud = 0
+call = 0
+put = 0
+pyramid = 0
+misleading = 0
+
+while case <= sufswitchcase:
+	try:
+		urllib.request.urlopen(url + str(case) + suffix2)
+	except urllib.error.URLError:
+		case += 1
+	else:
+		soup = BeautifulSoup(urllib.request.urlopen(url + str(case) + suffix2))
+		text = soup.get_text().lower()
+		option = text.count("option")
+		spindashoff = text.count("spin-off")
+		spinoff = text.count("spinoff")
+		spinspaceoff = text.count("spin off")	
+		spundashoff = text.count("spun-off")
+		spunoff = text.count("spunoff")
+		spunspaceoff = text.count("spun off")
+		insider = text.count("insider")
+		divest = text.count("divest")
+		fraud = text.count("fraud")
+		call = text.count("call")
+		put = text.count("put")
+		pyramid = text.count("pyramid")
+		misleading = text.count("misleading")													
+		logging.info("%d,option %d,spin-off %d,spinoff %d,spin off %d,spun-off %d,spunoff %d,spun off %d,insider %d,divest %d,fraud %d,call %d,put %d,pyramid %d,misleading %d" % (case, option, spindashoff, spinoff, spinspaceoff, spundashoff, spunoff, spunspaceoff, insider, divest, fraud, call, put, pyramid, misleading))
+		print(case)
+		case += 1	
+while case <= sufswitch2:
 	try:
 		urllib.request.urlopen(url + str(case) + suffix)
 	except urllib.error.URLError:
@@ -74,17 +108,28 @@ while case <= newestcase:
 		logging.info("%d,option %d,spin-off %d,spinoff %d,insider %d,divest %d,fraud %d,call %d,put %d,pyramid %d,misleading %d" % (case, option, spindashoff, spinoff, insider, divest, fraud, call, put, pyramid, misleading))
 		print(case)
 		case += 1
+while case <= newestcase:
+	try:
+		urllib.request.urlopen(url + str(case) + suffix)
+	except urllib.error.URLError:
+		case += 1
+	else:
+		soup = BeautifulSoup(urllib.request.urlopen(url + str(case) + suffix))
+		text = soup.get_text().lower()
+		option = text.count("option")
+		spindashoff = text.count("spin-off")
+		spinoff = text.count("spinoff")
+		insider = text.count("insider")
+		divest = text.count("divest")
+		fraud = text.count("fraud")
+		call = text.count("call")
+		put = text.count("put")
+		pyramid = text.count("pyramid")
+		misleading = text.count("misleading")														
+		logging.info("%d,option %d,spin-off %d,spinoff %d,insider %d,divest %d,fraud %d,call %d,put %d,pyramid %d,misleading %d" % (case, option, spindashoff, spinoff, insider, divest, fraud, call, put, pyramid, misleading))
+		print(case)
+		case += 1	
 while case <= ohsix:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2006/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -106,16 +151,6 @@ while case <= ohsix:
 		print(case)
 		case += 1	
 while case <= ohseven:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2007/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -137,16 +172,6 @@ while case <= ohseven:
 		print(case)
 		case += 1	
 while case <= oheight:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2008/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -168,16 +193,6 @@ while case <= oheight:
 		print(case)
 		case += 1	
 while case <= ohnine:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2009/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -199,16 +214,6 @@ while case <= ohnine:
 		print(case)
 		case += 1	
 while case <= ten:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2010/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -230,16 +235,6 @@ while case <= ten:
 		print(case)
 		case += 1	
 while case <= eleven:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2011/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -261,16 +256,6 @@ while case <= eleven:
 		print(case)
 		case += 1	
 while case <= twelve:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2012/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -292,16 +277,6 @@ while case <= twelve:
 		print(case)
 		case += 1
 while case <= thirteen:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2013/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -323,16 +298,6 @@ while case <= thirteen:
 		print(case)
 		case += 1	
 while case <= fourteen:
-	option = 0
-	spindashoff = 0
-	spinoff = 0
-	insider = 0
-	divest = 0
-	fraud = 0
-	call = 0
-	put = 0
-	pyramid = 0
-	misleading = 0
 	try:
 		urllib.request.urlopen(url2 + "2014/lr" + str(case) + suffix)
 	except urllib.error.URLError:
@@ -353,4 +318,4 @@ while case <= fourteen:
 		logging.info("%d,option %d,spin-off %d,spinoff %d,insider %d,divest %d,fraud %d,call %d,put %d,pyramid %d,misleading %d" % (case, option, spindashoff, spinoff, insider, divest, fraud, call, put, pyramid, misleading))
 		print(case)
 		case += 1		
-print("DONE!")
+logging.info("DONE!")
